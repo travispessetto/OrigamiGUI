32c71dbbc815ea4b4eb11725f46d4a14eb135fa7
Travis Pessetto <travis.pessetto@aggiemail.usu.edu>
2017-08-15 00:23:45 -0600

    Remove v from version so it does not create something like vv1.0.0

--- debian/create-deb.sh
+++ debian/create-deb.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 cd ./debian
 version=`cat ../VERSION`
-mkdir origami-smtp_v$version
-mkdir origami-smtp_v$version/DEBIAN
-mkdir -p origami-smtp_v$version/etc/menu
-mkdir -p origami-smtp_v$version/usr/bin
-mkdir -p origami-smtp_v$version/etc/ssl/certs
-mkdir -p origami-smtp_v$version/usr/share/applications
+mkdir origami-smtp_$version
+mkdir origami-smtp_$version/DEBIAN
+mkdir -p origami-smtp_$version/etc/menu
+mkdir -p origami-smtp_$version/usr/bin
+mkdir -p origami-smtp_$version/etc/ssl/certs
+mkdir -p origami-smtp_$version/usr/share/applications
 cp ../Origami\ SMTP.jar ./Origami.SMTP.jar
 ./jar2sh.sh linux-launch.config
-cp Origami.SMTP.jar origami-smtp_v$version/usr/bin/Origami.SMTP.jar
-cp Origami.SMTP.sh origami-smtp_v$version/usr/bin/Origami.SMTP.sh
-cp debian-control origami-smtp_v$version/DEBIAN/control
-cp origami-smtp.desktop origami-smtp_v$version/usr/share/applications/origami-smtp.desktop
-cat ../Origami_CA.crt > origami-smtp_v$version/etc/ssl/certs/Origami_CA.pem
-dpkg-deb --build origami-smtp_v$version
-rm -rf origami-smtp_v1.0.0$version
-mv origami-smtp_v$version.deb ../
+cp Origami.SMTP.jar origami-smtp_$version/usr/bin/Origami.SMTP.jar
+cp Origami.SMTP.sh origami-smtp_$version/usr/bin/Origami.SMTP.sh
+cp debian-control origami-smtp_$version/DEBIAN/control
+cp origami-smtp.desktop origami-smtp_$version/usr/share/applications/origami-smtp.desktop
+cat ../Origami_CA.crt > origami-smtp_$version/etc/ssl/certs/Origami_CA.pem
+dpkg-deb --build origami-smtp_$version
+rm -rf origami-smtp_$version
+mv origami-smtp_$version.deb ../
 cd ..
-if test -e "origami-smtp_v$version.deb"; then
-	echo "origami-smtp_v$version.deb created"
+if test -e "origami-smtp_$version.deb"; then
+	echo "origami-smtp_$version.deb created"
     return 0
 else
     return 1
