5cbbe68075d0ed2be9eff9cd7d172d7aa9e650fb
Travis Pessetto <travis.pessetto@aggiemail.usu.edu>
2017-08-14 22:51:23 -0600

    Organize debian release

---
+++ debian/create-deb.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+version=`cat ../VERSION`
+mkdir origami-smtp_v$version
+mkdir origami-smtp_v$version/DEBIAN
+mkdir -p origami-smtp_v$version/etc/menu
+mkdir -p origami-smtp_v$version/usr/bin
+mkdir -p origami-smtp_v$version/etc/ssl/certs
+mkdir -p origami-smtp_v$version/usr/share/applications
+cp ../Origami\ SMTP.jar ./Origami.SMTP.jar
+./jar2sh.sh linux-launch.config
+cp Origami.SMTP.jar origami-smtp_v$version/usr/bin/Origami.SMTP.jar
+cp Origami.SMTP.sh origami-smtp_v$version/usr/bin/Origami.SMTP.sh
+cp debian-control origami-smtp_v$version/DEBIAN/control
+cp origami-smtp.desktop origami-smtp_v$version/usr/share/applications/origami-smtp.desktop
+cat Origami_CA.crt > origami-smtp_v$version/etc/ssl/certs/Origami_CA.pem
+dpkg-deb --build origami-smtp_v$version
+rm -rf origami-smtp_v1.0.0$version
+mv origami-smtp_v$version.deb ../
+if -e "../origami-smtp_v$version.deb; then
+    return 0
+else
+    return 1
+fi
