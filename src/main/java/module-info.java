43ffd816c7f35037b144cde4075d7c42493b103d
Travis Pessetto <travis@pessetto.com>
2022-04-20 19:52:14 -0600

    Starting to modularize project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module OrigamiGUI {
+    requires javafx.controls;
+    requires javafx.web;
+    requires javafx.fxml;
+    requires javafx.swing;
+    requires javafx.base;
+    requires jdk.jsobject;
+    requires java.desktop;
+
+    opens com.pessetto.origamigui to javafx.fxml, java.desktop;
+    opens com.pessetto.origamigui.web to javafx.web;
+    exports com.pessetto.origamigui.console;
+}
