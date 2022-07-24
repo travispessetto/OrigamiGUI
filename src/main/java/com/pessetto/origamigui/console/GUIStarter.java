4bab3840388c0148c3dd4928d289361949126aed
Travis Pessetto <travis@pessetto.com>
2022-07-23 18:30:39 -0600

    Make it so that we do not have to add JavaFX runtimes to command line

---
+++ src/main/java/com/pessetto/origamigui/console/GUIStarter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pessetto.origamigui.console;
+
+/**
+ * Allows JavaFX to start via the command line java -jar
+ * @author Travis Pessetto
+ */
+public class GUIStarter {
+    public static void main(final String[] args)
+    {
+        OrigamiGUI.main(args);
+    }
+}
